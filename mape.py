2f28c539d35da0a040fa5f81933ae7e277ec683e
Pokecreatorfr <64284499+Pokecreatorfr@users.noreply.github.com>
2021-04-14 18:25:21 +0200

    python

--- mape.py
+++ mape.py
@@ -1,6 +1,16 @@
 from romh import *
 from Value import *
+from mapextractor import *
 
-def depoint(point,x):
+def depointl(point,x):
     hexv = point[x]
     point[x] = hexv[4:6] + hexv [2:4] + hexv[0:2]
+
+def ajouthexl(list,x,y):
+    for i in range(x):
+        hexv = conv_hex2dec(list[x])+y
+        list[x] = conv_dec2hex(hexv)
+
+def readbank(rom):
+    for i in range(43):
+        bankp[i] = readRomData(rom, listadre[i], 3)
--- mapextractor.py
+++ mapextractor.py
@@ -4,11 +4,10 @@ from Value import *
 filename = "BPRE0.gba"
 
 
+
 hexrom = openRomRead(filename)
-for i in range (0,43):
+for i in range(43):
     bankp[i] = readRomData(hexrom, listadre[i], 3)
-    for x in range (0,43):
-        depoint(bankp,x)
-
-
+    depointl(bankp,i)
+    bankp[i] = conv_hex2dec(bankp[i])
 print(bankp)
