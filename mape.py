6b3365bf929eb608ceff5c6257c889980947a59c
Pokecreatorfr <pokecratorfr@gmail.com>
2021-11-12 14:58:43 +0100

    Update mape.py

--- mape.py
+++ mape.py
@@ -111,9 +111,9 @@ class mapformat:
         self.block = readRomData(fichiermap, 28, 12).decode(encoding="utf-8")
         #Connection 
         varhex = readRomByte(fichiermap, conv_dec2hex((int(len(fichiermap)/2)-2))).decode(encoding="utf-8") + readRomByte(fichiermap, conv_dec2hex((int(len(fichiermap)/2)-3))).decode(encoding="utf-8") + readRomByte(fichiermap, conv_dec2hex((int(len(fichiermap)/2)-4))).decode(encoding="utf-8")
-        self.connection = readRomData(fichiermap, varhex, (int(len(fichiermap)/2)-4) - conv_hex2dec(varhex)).decode(encoding="utf-8")
-        if self.connexions == '':
-            self.noconnection = True
+        self.connexion = readRomData(fichiermap, varhex, (int(len(fichiermap)/2)-4) - conv_hex2dec(varhex)).decode(encoding="utf-8")
+        if self.connexion == '':
+            self.noconnexion = True
         #Scripts
         self.nbscriptpnj = readRomByte(fichiermap, readRomByte(fichiermap, '0x1D').decode(encoding="utf-8")+readRomByte(fichiermap, '0x1C').decode(encoding="utf-8")).decode(encoding="utf-8")
         self.nbwarp = readRomByte(fichiermap, add2hex((readRomByte(fichiermap, '0x1D').decode(encoding="utf-8")+readRomByte(fichiermap, '0x1C').decode(encoding="utf-8")), 1)).decode(encoding="utf-8")
